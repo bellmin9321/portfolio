919281547ab70ecba96c3fe7d03bac35770af3d0
bellmin9321 <bellmin9321@gmail.com>
2021-04-24 18:10:50 +0900

    practice main.js

---
+++ main1.js
@@ -0,0 +1,63 @@
+'use strict'
+
+// Make navbar transparent when it is on the top
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+  console.log(window.scrollY);
+  console.log(`navbarHeight : ${navbarHeight}`);
+
+  if(window.scrollY > navbarHeight) {
+    navbar.classList.add('navbar--dark');
+  } else {
+    navbar.classList.remove('navbar--dark')
+  }
+})
+
+// Home
+const navHome = document.querySelector('.navHome');
+const home = document.querySelector('#home');
+
+navHome.addEventListener('click', () => {
+  home.scrollIntoView();
+})
+
+// About
+const navAbout = document.querySelector('.navAbout');
+const about = document.querySelector('#about');
+
+navAbout.addEventListener('click', () => {
+  about.scrollIntoView();
+})
+// Skills
+const navSkills = document.querySelector('.navSkills');
+const skills = document.querySelector('#skills');
+
+navSkills.addEventListener('click', () => {
+  skills.scrollIntoView();
+})
+// My work
+const navWork = document.querySelector('.navWork');
+const work = document.querySelector('#work');
+
+navWork.addEventListener('click', () => {
+  work.scrollIntoView();
+})
+
+// Testimonial
+const navTestimonial = document.querySelector('.navTestimonial');
+const testimonial = document.querySelector('#testimonial');
+
+navTestimonial.addEventListener('click', () => {
+  testimonial.scrollIntoView();
+  console.log('testimonial ')
+})
+
+
+// Contact
+const navContact = document.querySelector('.navContact');
+const contact = document.querySelector('#contact');
+
+navContact.addEventListener('click', () => {
+  contact.scrollIntoView();
+})
