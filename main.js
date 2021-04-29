30cc119c9cac54ab2a297e62a2a227491bf5cdcb
bellmin9321 <bellmin9321@gmail.com>
2021-04-29 22:22:01 +0900

    js arrow-up

--- main.js
+++ main.js
@@ -41,6 +41,21 @@ document.addEventListener('scroll', () => {
   }
 )
 
+// show "arrow up" button when scrolling down
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+  if(window.scrollY > homeHeight /2 ) {
+    arrowUp.classList.add('visible');
+  } else {
+    arrowUp.classList.remove('visible');
+  }
+})
+
+// Handle click on the "arrow up" button
+arrowUp.addEventListener('click', () => {
+  scrollIntoView('#home')
+})
+
 // function scrollIntoVeiw -> 반복되는 함수 방지
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
