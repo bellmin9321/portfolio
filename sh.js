94a0a92e482da79c9f4ebb88061fdd4ce4c2a8e4
bellmin9321 <bellmin9321@gmail.com>
2021-08-17 21:57:54 +0900

    add class > const function

---
+++ sh.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const list = document.querySelector(".list"),
+  input = document.querySelector(".send_text"),
+  delete_btn = document.querySelector(".delete_btn");
+
+function
+
+
+
+
