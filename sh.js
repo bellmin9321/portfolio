25b3d9bad254aa80e7ecf08cc66a1ed53993d22a
bellmin9321 <bellmin9321@gmail.com>
2021-08-18 23:10:12 +0900

    add addItem

--- sh.js
+++ sh.js
@@ -1,10 +1,23 @@
 'use strict'
 
+const itemList = [];
 const list = document.querySelector(".list"),
   input = document.querySelector(".send_text"),
-  delete_btn = document.querySelector(".delete_btn");
+  delete_btn = document.querySelector(".delete_btn"),
+  inputButton = document.querySelector(".input_button");
 
-function
+function addItem() {
+  const item = document.querySelector(".item").value;
+  if (item != null) {
+    list.push(item) {
+      document.querySelector(".item").value = "";
+      document.querySelector(".item").foucs();
+    }
+
+    showList()
+  }
+}
+
 
 
 
