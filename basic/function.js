3c286672792c58f507586e8545047261e78f9fb7
bellmin9321 <bellmin9321@gmail.com>
2021-08-30 23:53:24 +0900

    study basic function

---
+++ basic/function.js
@@ -0,0 +1,15 @@
+const num = 1;
+const num2 = 2;
+const result = num + num2;
+console.log(result);
+
+const num3 = 1;
+const num4 = 2;
+const result2 = num3 + num4;
+
+function add(bab, aba) {
+  return bab + aba;
+}
+
+const sum = add(3, 4);
+console.log(sum);
