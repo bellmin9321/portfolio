3204f49fe6f8da0d79f5b3ba99572769108cb918
bellmin9321 <bellmin9321@gmail.com>
2021-08-30 23:52:32 +0900

    study basic

---
+++ basic/obj.js
@@ -0,0 +1,20 @@
+
+  let obj = {
+    name: 'ellie',
+    age: 5,
+  };
+  console.log(obj.name);
+
+  let obj2 = obj;
+  console.log(obj2.name);
+
+  obj.name = 'bellmin'
+  console.log(obj2.name);
+
+  let a = 2;
+  a = 5;
+  a = 9;
+  console.log(a);
+
+  const num = 2;
+  
