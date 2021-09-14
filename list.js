50fadda145237c718b7f10eeec612db203d5dd7f
bellmin9321 <bellmin9321@gmail.com>
2021-09-14 21:47:20 +0900

    엘리꺼 보고 추가함

--- list.js
+++ list.js
@@ -6,12 +6,44 @@ const input = document.querySelector('.input'),
       item = document.querySelector('.item');
 
       // <script ... defer> 작성안하면 실행안됨
-addBtn.addEventListener("click", function() {
-  const newList = document.createElement('li');
-  newList.setAttribute('class', 'item')
-  list.appendChild(newList);
+function onAdd() {
+  const text = input.value
+  if (text === '') {
+    input.focus();
+    return;
+  }
+  
+const item = createItem(text);
+list.appendChild(item);
 
-  newList.innerText = input.value;
-  input.value = "";
-  console.log('DD')
+  item.addEventListener('click', () => {
+    item.style.textDecoration = "line-through";
+  })
+  item.scrollIntoView({block: 'center'});
+  input.focus();
+}
+
+let id = 0;
+function createItem(text) {
+  const row = document.createElement('li');
+  row.setAttribute('class', 'item__row');
+  row.setAttribute('data-id', id);
+  row.innerHTML = `
+  <div class="item">
+    <span class="item__name">${text}</span>
+    <button><i class="fas fa-minus" data-id=${id}></i></button>
+  </div>
+  `
+  id++;
+  return row;
+}
+
+addBtn.addEventListener("click", () => {
+  onAdd();
+})
+
+input.addEventListener('keypress', (event) => {
+  if(event.key === 'Enter') {
+    onAdd();
+  }
 })
