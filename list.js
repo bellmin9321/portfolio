1998dd98321ea284fbb5f35bfa5842b7716678d2
bellmin9321 <bellmin9321@gmail.com>
2021-09-15 21:45:29 +0900

    update

--- list.js
+++ list.js
@@ -2,8 +2,10 @@
 
 const input = document.querySelector('.input'),
       addBtn = document.querySelector('.button__add'),
-      list = document.querySelector('.item__list'),
-      item = document.querySelector('.item');
+      items = document.querySelector('.item__list'),
+      item = document.querySelector('.item'),
+      itemRow = document.querySelector('.item__row'),
+      deleteBtn = document.querySelector('.delete__btn');
 
       // <script ... defer> 작성안하면 실행안됨
 function onAdd() {
@@ -14,28 +16,30 @@ function onAdd() {
   }
   
 const item = createItem(text);
-list.appendChild(item);
+itemRow.appendChild(item);
 
   item.addEventListener('click', () => {
     item.style.textDecoration = "line-through";
   })
+
   item.scrollIntoView({block: 'center'});
+  input.value = '';
   input.focus();
 }
 
 let id = 0;
 function createItem(text) {
-  const row = document.createElement('li');
-  row.setAttribute('class', 'item__row');
-  row.setAttribute('data-id', id);
-  row.innerHTML = `
+  const itemRow = document.createElement('li');
+  itemRow.setAttribute('class', 'item__row');
+  itemRow.setAttribute('data-id', id);
+  itemRow.innerHTML = `
   <div class="item">
     <span class="item__name">${text}</span>
-    <button><i class="fas fa-minus" data-id=${id}></i></button>
+    <button class = "delete__btn"><i class="fas fa-minus" data-id=${id}></i></button>
   </div>
-  `
+  `;
   id++;
-  return row;
+  return itemRow;
 }
 
 addBtn.addEventListener("click", () => {
@@ -47,3 +51,12 @@ input.addEventListener('keypress', (event) => {
     onAdd();
   }
 })
+
+itemRow.addEventListener('click', (event) => {
+  const id = event.target.dataset.id;
+  if(id) {
+    const deleteItem = document.querySelector(`.item[data-id="${id}]`);
+    deleteItem.remove();
+    }
+})
+
